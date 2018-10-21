2a07b2dd0cd0a5fcd4b01f2f6ab91332f0f69ef6
zscgrhg <zscgrhg@qq.com>
2018-10-21 16:49:11 +0800

    con

---
+++ src/main/java/com/zte/crm/chromie/contract/WebConfiguration.java
@@ -0,0 +1,28 @@
+package com.zte.crm.chromie.contract;
+
+import org.springframework.boot.autoconfigure.web.WebMvcRegistrations;
+import org.springframework.boot.autoconfigure.web.WebMvcRegistrationsAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@Configuration
+public class WebConfiguration {
+
+    @Bean
+    public WebMvcRegistrations mappingOnlyOnController() {
+        return new WebMvcRegistrationsAdapter() {
+            @Override
+            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+                return new RequestMappingHandlerMapping() {
+                    protected boolean isHandler(Class<?> beanType) {
+                        return (AnnotatedElementUtils.hasAnnotation(beanType, Controller.class));
+                    }
+                };
+            }
+        };
+    }
+
+}
